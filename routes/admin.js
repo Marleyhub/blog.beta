2c066179060e41a8b193f1a3654df360b2846328
Gabriel Marley <cardoso_eng@outlook.com>
2022-05-13 00:11:35 -0300

    adding a comentary on the admin.js error

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,7 @@ router.get('/cat', (req,res)=>{
     res.send('pagina de categorias')
 })
 router.get('/', (req,res)=>{
-   res.render("admin/index")
+   res.render("admin/index") // nessa parte aqui não quer renderizar, por algum motivo a função render não está achando a pasta "admin" pra renderizar o arquivo "index.hendlebars" está dando o erro "Error: ENOENT: no such file or directory, open '/home/gabriel/Documents/project/views/layouts/main.handlebars'"
 })
 
 module.exports = router
