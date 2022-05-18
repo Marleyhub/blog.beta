12bcb316d433010eb802f5216751bf79d8757cef
Gabriel Marley <cardoso_eng@outlook.com>
2022-05-18 02:34:20 -0300

    adding new features (form, .flash and middleware manipulation)

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require ('express')
 const handlebars = require ('express-handlebars')
-const res = require('express/lib/response')
-//const mongoose = require ('mongoose')
+const mongoose = require ('mongoose')
 //const bodyparser = require ('body-parser')
 const app = express()
 const path = require ("path")
 const admin = require('./routes/admin')
+const session = require ('express-session')
+const flash = require ('connect-flash')
 
 
+// moogose
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://127.0.0.1:27017/blogapp").then(()=>{
+   console.log('conectado ao Mongo')
+}).catch((err)=>{
+   console.log("erro ao conectar: Mongo" +err)  
+})
 // tamplate engine 
 app.engine('handlebars', handlebars.engine({defaultlayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -15,7 +23,22 @@ app.set('view engine', 'handlebars')
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
-// rotas
+//session(middleware ultilizada para manipular dados de requisições ou envio de dados do servidor)
+app.use(session({
+   secret: 'cursodenode',
+   resave: true,
+   saveUninitialized: true
+}))
+//flash (apresenta aplicações temporarias)
+app.use(flash())
+// ultilizando variaveis globais em uma middleware
+app.use((req,res,next)=>{
+   res.locals.sucess_msg = req.flash('success_msg')
+   res.locals.error_msg = req.flash('error_msg')
+   next()
+})
+
+// rotas config   
 app.get ('/', (req,res)=>{
    res.send('home')
 })
@@ -27,6 +50,7 @@ app.get('posts', (req,res)=>{
 app.use('/admin', admin)
 //referenciando local de arquivos estáticos 
 app.use(express.static(path.join(__dirname,"public")))
+
 
 const Port = 3002
 app.listen (Port,() =>{
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,49 @@
 const express = require ('express')
 const router = express.Router()
+// mongo 
+const mongoose = require ('mongoose')
+require ('../models/Categoria')
+// referenciando um model a uma constante 
+const Categoria = mongoose.model('categorias')
 
+// rotas
 router.get('/posts', (req,res)=>{
     res.send('pagina de posts')
 })
-router.get('/cat', (req,res)=>{
-    res.send('pagina de categorias')
+router.get('/categorias', (req,res)=>{
+    res.render('admin/categorias')
 })
 router.get('/', (req,res)=>{
-   res.render("index") // nessa parte aqui não quer renderizar, por algum motivo a função render não está achando a pasta "admin" pra renderizar o arquivo "index.hendlebars" está dando o erro "Error: ENOENT: no such file or directory, open '/home/gabriel/Documents/project/views/layouts/main.handlebars'"
+   res.render("admin/index") 
 })
+router.get('/categorias/add', (req,res)=>{
+    res.render("admin/addcategorias")
+})
+// limitandoi possibilidades do usuário em criar nomes e slugs com me
+router.post('/categorias/nova', (req,res)=>{
 
+    var erros = []
+
+    if(!req.body.name || typeof req.body.name == undefined || req.body.name == null){
+        erros.push({texto: "Nome invalido"})
+    }
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.name == null){
+        erros.push({texto: "Slug invalida"})
+    }
+    if(erros.length > 0){
+        res.render("admin/addcategorias",{erros: erros})
+    }else{
+    const novaCategoria = {
+        nome: req.body.name,
+        slug: req.body.slug
+}
+    new Categoria(novaCategoria).save().then(()=>{
+        req.flash('success_msg','Categoria criada com sucesso')
+        res.redirect("/admin/categorias")
+    }).catch((err)=>{
+        req.flash('error_msg','Houve um erro na criação da categoria' )
+        res.redirect("/admin")
+    })
+}
+})
 module.exports = router
