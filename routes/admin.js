d8959a6d4b0afa27c7d65da2c9e940cf49287cc5
Gabriel Marley <cardoso_eng@outlook.com>
2022-07-28 19:17:00 -0300

    organizando

--- routes/admin.js
+++ routes/admin.js
@@ -57,9 +57,9 @@ router.post('/categorias/edit/',(req,res)=>{
     }).catch((err)=>{
         req.flash("error_msg", "Houve um erro ao editar a categoria")
         res.redirect('/admin/categorias')
-       
     })
 })
+
 //deletando categoria
 router.post('/categorias/delete', (req,res) =>{
     Categoria.remove({_id: req.body.id}).then(()=>{
@@ -72,7 +72,8 @@ router.post('/categorias/delete', (req,res) =>{
         console.log('Nao foi ' +err)
     })
 })
-// limitandoi possibilidades do usuário em criar nomes e slugs com me
+
+//limitandoi possibilidades do usuário em criar nomes e slugs com me
 router.post('/categorias/nova', (req,res)=>{
 
     var erros = []
@@ -100,6 +101,7 @@ router.post('/categorias/nova', (req,res)=>{
     })
 }
 })
+
 //listando postagens
 router.get('/postagens', (req,res)=>{
     Postagem.find().populate("categoria").sort({date:"desc"}).lean().then((postagens)=>{
@@ -111,6 +113,7 @@ router.get('/postagens', (req,res)=>{
     })
    
 })
+
 // listando categorias dentro da view addpostagens.handlebars
 router.get('/postagem/add',(req,res) =>{
     Categoria.find().lean().then((categoria)=>{
@@ -120,6 +123,7 @@ router.get('/postagem/add',(req,res) =>{
         res.redirect('admin')   
     })  
 })
+
 //salvando postagem
 router.post('/postagem/nova',(req,res)=>{
     var erros = []
@@ -135,7 +139,7 @@ router.post('/postagem/nova',(req,res)=>{
             categoria: req.body.categoria,
             descricao: req.body.descricao,
             conteudo: req.body.conteudo
-            // os nomes estavam incompatíveis com o fomulário criado em "addpostagens" sendo assim os dados não eram salvos no banco de dados pois o que estavam com os nomes incompatíveis simplesmente não salvavam e todos os itens do objeto Postagem são obrigatórios pelo "required"
+// os nomes estavam incompatíveis com o fomulário criado em "addpostagens" sendo assim os dados não eram salvos no banco de dados pois o que estavam com os nomes incompatíveis simplesmente não salvavam e todos os itens do objeto Postagem são obrigatórios pelo "required"
         }
             new Postagem(novaPostagem).save().then(()=>{
             req.flash ('success_msg','categoria salva com sucesso')
@@ -147,11 +151,14 @@ router.post('/postagem/nova',(req,res)=>{
         })
     }
 })
+
 //Abrindo a view de edicão de postagens 
-router.get('/postagem/edit/:id', (req,res)=>{ // a identifição por id vem por meio dessa rota
+//a identifição por id vem por meio dessa rota:
+router.get('/postagem/edit/:id', (req,res)=>{ 
     Postagem.findOne({_id: req.params.id}).lean().then((postagem)=>{
      Categoria.find().lean().then((categorias)=>{  
-     res.render('admin/editpostagem',{categorias: categorias, postagem: postagem})// colocar este trecho sem a barra no começo faz o sistema entender como um arquivo dentro da view
+//colocar este trecho sem a barra no começo faz o sistema entender como um arquivo dentro da view:
+     res.render('admin/editpostagem',{categorias: categorias, postagem: postagem})
     })
 })
 })
@@ -159,7 +166,8 @@ router.get('/postagem/edit/:id', (req,res)=>{ // a identifição por id vem por 
 //editando postagem
 router.post('/postagem/edit/', (req,res)=>{
     Postagem.findOne({_id: req.body.id}).then((postagem)=>{
-        
+// nessa parte o sistema usa dados do id e nome do input criado na view:
+       
         postagem.titulo = req.body.titulo,
         postagem.slug = req.body.slug,
         postagem.descricao = req.body.descricao,
@@ -170,7 +178,7 @@ router.post('/postagem/edit/', (req,res)=>{
             req.flash("success_msg", 'Postagem salva')
             res.redirect('/admin/postagens')
         }).catch((err)=>{
-            req.flash('Erro ao salvar postagem')
+            req.flash('error_msg','Erro ao salvar postagem')
             res.redirect('/admin/postagens')
             console.log('não foi salvo')
             console.log(err)
@@ -180,9 +188,9 @@ router.post('/postagem/edit/', (req,res)=>{
         res.redirect('/admin/postagens')
         console.log(err)
     })
-    
 })
-// deletando postagem
+
+//deletando postagem
 router.post ('/postagem/delete', (req,res) =>{
     Postagem.remove({_id: req.body.id}).then(()=>{
         req.flash('success_msg', 'Postagem deletada')
